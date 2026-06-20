f7d656c8620f620b3bb0281eb5cd1e799d1e79cd
vaishnavich4 <vyshnavich4@gmail.com>
2026-06-20 12:16:03 +0530

    Refactor app.py to include os import and formatting

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, session, url_for
 import mysql.connector
 from datetime import date, timedelta
+import os
 
 app = Flask(__name__)
 app.secret_key = "medtrack123"
